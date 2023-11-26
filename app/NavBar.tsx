befcda4f121769bc8119df4e777e2294f1d70414
saravanakumar r <saravanakumar.ravichandran@oracle.com>
2023-11-26 16:07:42 +0530

    Adding next auth session and authprovider

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,13 +1,23 @@
+"use client";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
+import AuthProvider from "./auth/provider";
 
 const NavBar = () => {
+  const { status, data: session } = useSession();
+  //if (status === "loading") return null;
   return (
-    <div className="bg-slate-200 p-5">
+    <div className="flex bg-slate-200 p-3 space-x-3">
       <Link href="/" className="mr-5">
         Next.js
       </Link>
       <Link href="/users">Users</Link>
+      {status === "loading" && <div>Loading...</div>}
+      {status === "authenticated" && <div>{session.user!.name}</div>}
+      {status === "unauthenticated" && (
+        <Link href="/api/auth/signin">Login</Link>
+      )}
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
 import { Suspense } from "react";
+import AuthProvider from "./auth/provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,8 +20,10 @@ export default function RootLayout({
   return (
     <html data-theme="winter" lang="en">
       <body className={inter.className}>
-        <NavBar />
-        <main className="p-5">{children}</main>
+        <AuthProvider>
+          <NavBar />
+          <main className="p-5">{children}</main>
+        </AuthProvider>
       </body>
     </html>
   );
