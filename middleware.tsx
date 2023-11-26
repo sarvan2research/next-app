360c34bbb57abd51e27218dcd11e8f67358d68e0
saravanakumar r <saravanakumar.ravichandran@oracle.com>
2023-11-26 17:03:00 +0530

    Restricting access using nextauth middleware

--- middleware.tsx
+++ middleware.tsx
@@ -1,9 +1,13 @@
-import { NextRequest, NextResponse } from "next/server";
+// Default next auth implementation
+export { default } from "next-auth/middleware";
 
-// middleware is casesensitive file used to redirect  or control page access request based on certain params
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/new-page", request.url));
-}
+//import { middleware } from "next-auth/middleware";
+// export default middleware
+
+// // middleware is casesensitive file used to redirect  or control page access request based on certain params
+// export function middleware(request: NextRequest) {
+//   return NextResponse.redirect(new URL("/new-page", request.url));
+// }
 
 export const config = {
   // * zero or more
